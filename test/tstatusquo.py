02134ead8436e86c139b705999f3099940fe0afa
abrantedevops <abrante.devops@gmail.com>
2023-06-10 13:05:22 -0300

    devpip11

---
+++ test/tstatusquo.py
@@ -0,0 +1,131 @@
+import subprocess
+import time
+
+def test_docker_web():
+    containers = [
+        {"name": "statusquo-apache"},
+    ]
+    for container in containers:
+        print("========================================= APACHE =========================================")
+        print()
+        print("Instalando dependências no container: " + "\033[91m"+container["name"]+"\033[0m")
+        subprocess.run(["docker", "exec", container["name"], "/bin/sh", "-c", "apt install curl -y"])
+        print()
+        print("Iniciando o Teste das Requisições LAN e WAN")
+        print()
+        loading_text = "Carregando"
+        for _ in range(5):
+            print(loading_text, end="", flush=True)
+            loading_text += "."
+            time.sleep(0.5)  
+            print("\r", end="") 
+        print()
+        print()
+        print("Domínio (app01 - lan): http://app01.abranteme.com.br")
+        command = ["docker", "exec", container["name"], "/bin/sh", "-c", "curl app01.abranteme.com.br"]
+        result = subprocess.run(command, capture_output=True, text=True)
+        print(result.stdout)
+        print()
+        if result.returncode == 0:
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print("\033[92mFinished. No error\033[0m")  
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print()
+        else:
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print("\033[91mAttention! app01.abranteme.com.br with error.\033[0m")
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print()
+        print("Domínio (app02 - lan): http://app02.abranteme.com.br")
+        command = ["docker", "exec", container["name"], "/bin/sh", "-c", "curl app02.abranteme.com.br"]
+        result = subprocess.run(command, capture_output=True, text=True)
+        print(result.stdout)
+        print()
+        if result.returncode == 0:
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print("\033[92mFinished. No error\033[0m")  
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print()
+        else:
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print("\033[91mAttention! app02.abranteme.com.br with error.\033[0m")
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print()
+        print("Domínio (app02/jornal - lan): http://app02.abranteme.com.br/jornal")
+        command = ["docker", "exec", container["name"], "/bin/sh", "-c", "curl app02.abranteme.com.br/jornal"]
+        result = subprocess.run(command, capture_output=True, text=True)
+        print(result.stdout)
+        print()
+        if result.returncode == 0:
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print("\033[92mFinished. No error\033[0m")  
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print()
+        else:
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print("\033[91mAttention! app01.abranteme.com.br/jornal with error.\033[0m")
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print()
+        print("Domínio (app01 - wan): https://app01.abranteme.com.br")
+        command = ["docker", "exec", container["name"], "/bin/sh", "-c", "curl -k https://app01.abranteme.com.br"]
+        result = subprocess.run(command, capture_output=True, text=True)
+        print(result.stdout)
+        print()
+        if result.returncode == 0:
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print("\033[92mFinished. No error\033[0m")  
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print()
+        else:
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print("\033[91mAttention! app01.abranteme.com.br with error.\033[0m")
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print()
+        print("Domínio (app02 - wan): https://app02.abranteme.com.br")
+        command = ["docker", "exec", container["name"], "/bin/sh", "-c", "curl -k https://app02.abranteme.com.br"]
+        result = subprocess.run(command, capture_output=True, text=True)
+        print(result.stdout)
+        print()
+        if result.returncode == 0:
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print("\033[92mFinished. No error\033[0m")  
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print()
+        else:
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print("\033[91mAttention! app02.abranteme.com.br with error.\033[0m")
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print()
+        print("Domínio (app02/jornal - wan): https://app02.abranteme.com.br/jornal")
+        command = ["docker", "exec", container["name"], "/bin/sh", "-c", "curl -k https://app02.abranteme.com.br/jornal"]
+        result = subprocess.run(command, capture_output=True, text=True)
+        print(result.stdout)
+        print()
+        if result.returncode == 0:
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print("\033[92mFinished. No error\033[0m")  
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print()
+        else:
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print("\033[91mAttention! app01.abranteme.com.br/jornal with error.\033[0m")
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print()
+
+# def stop_docker_container():
+#     containers = ["statusquo-apache"]
+#     for container in containers:
+#         subprocess.run(["docker", "stop", container])
+
+# def remove_docker_container():
+#     containers = ["statusquo-apache"]
+#     for container in containers:
+#         subprocess.run(["docker", "rm", container])
+
+
+
+test_docker_web()
+# stop_docker_container()
+# remove_docker_container()
+
+
---
+++ test/tveridis.py
@@ -0,0 +1,86 @@
+import subprocess
+
+def test_docker_dns():
+    containers = [
+        {"name": "veridis-bindm"},
+        {"name": "veridis-binds"}
+    ]
+    for container in containers:
+        print("========================================= BIND9 =========================================")
+        print()
+        print("Verificando o container do Servidor DNS: " + "\033[91m"+container["name"]+"\033[0m")
+        command = ["docker", "exec", container["name"], "/bin/sh", "-c", "nslookup app01.abranteme.com.br"]
+        result = subprocess.run(command, capture_output=True, text=True)
+        print(result.stdout)
+        print()
+        if result.returncode == 0:
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print("\033[92mFinished. No error\033[0m")  
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print()
+        else:
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print("\033[91mAttention! BIND9 with error.\033[0m")
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print()
+# IMPLEMENTAR OS LOGS DO BIND(9)
+
+def test_docker_nginx():
+    containerslan = [
+        {"name": "nginx-lan"},
+    ]
+    for container in containerslan:
+        print("========================================= NGINX =========================================")
+        print("Verificando o container do Servidor Proxy: " + "\033[91m"+container["name"]+"\033[0m")
+        print()
+        command = ["docker", "exec", container["name"], "/bin/sh", "-c", "service nginx status"]
+        result = subprocess.run(command, capture_output=True, text=True)
+        print(result.stdout)
+        print()
+        if result.returncode == 0:
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print("\033[92mFinished. No error\033[0m")  
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print()
+        else:
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print("\033[91mAttention! Nginx with error.\033[0m")
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print()
+    
+    containerswan = [
+        {"name": "nginx-wan"},
+    ]
+    for container in containerswan:
+        print("Verificando o container do Servidor Proxy: " + "\033[91m"+container["name"]+"\033[0m")
+        print()
+        command = ["docker", "exec", container["name"], "/bin/sh", "-c", "service nginx status"]
+        result = subprocess.run(command, capture_output=True, text=True)
+        print(result.stdout)
+        print()
+        if result.returncode == 0:
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print("\033[92mFinished. No error\033[0m")  
+            print("\033[92m+-+-+-+-+-+-+-+-+-+\033[0m")
+            print()
+        else:
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print("\033[91mAttention! Nginx with error.\033[0m")
+            print("\033[91m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-\033[0m")
+            print()
+
+# def stop_docker_container():
+#     containers = ["veridis-bindm", "veridis-binds", "nginx-lan", "nginx-wan"]
+#     for container in containers:
+#         subprocess.run(["docker", "stop", container])
+
+# def remove_docker_container():
+#     containers = ["veridis-bindm", "veridis-binds", "nginx-lan", "nginx-wan"]
+#     for container in containers:
+#         subprocess.run(["docker", "rm", container])
+
+
+test_docker_dns()
+test_docker_nginx()
+# stop_docker_container()
+# remove_docker_container()
